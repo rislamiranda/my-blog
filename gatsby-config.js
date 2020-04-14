a205eee7dc67224ec0fb5c1f08f195413449d1f7
Risla Miranda <rislamiranda@gmail.com>
2020-04-14 16:49:13 -0300

    Start blog

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Risla Miranda`,
+    author: `Risla Miranda`,
+    description: `Transformando o processo em escrita.`,
+    siteUrl: "https://risla.dev",
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/rislamiranda`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/rislamiranda`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default () => (
+  <Fragment>
+    Mestra em Direitos Humanos, cientista de dados em construção e pesquisadora do <Styled.a href="http://arteaberta.com/">Arte Aberta</Styled.a>.
+    {` `}
+    
+    <br />
+  </Fragment>
+)
